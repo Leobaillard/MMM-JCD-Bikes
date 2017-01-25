5d4c3103e04b6d7ae077e0baabba4b90e3c21638
Léopold Baillard <leobaillard@leoserveur.org>
2017-01-25 16:28:26 +0100

    WIP: adding debug information

--- node_helper.js
+++ node_helper.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6*/
 /* Magic Mirror
  * Module: MMM-JCD-Bikes
  *
@@ -34,11 +35,15 @@ module.exports = NodeHelper.create({
 	
     socketNotificationReceived: function(notification, payload) {
         if(notification === 'CONFIG'){
+            console.log("[JCD Bikes] CONFIG event received");
             this.config = payload;
             this.returnData = {};
+            console.log("[JCD Bikes] Getting stations data");
             for ( var i = 0, len = this.config.stations.length; i < len; i++ )
             {
+                console.log("[JCD Bikes] --> Station " + this.config.stations[i] + "...");
                 var api_url = this.config.apiBase + 'stations/' + this.config.stations[i] + this.getParams();
+                console.log("[JCD Bikes] --> URL : " + api_url);
                 this.getData(api_url);
             }
             this.sendSocketNotification("JCD BIKES", this.returnData);
@@ -53,11 +58,12 @@ module.exports = NodeHelper.create({
 	
     getData: function(options) {
 		request(options, (error, response, body) => {
+            console.log("[JCD Bikes] --> Request made (" + response.statusCode + ")");
 	        if (response.statusCode === 200) {
                 var parsedStation = this.parseData(body);
                 this.returnData[parsedStation.number] = parsedStation;
             } else {
-                console.log("Error getting nextbike data " + response.statusCode);
+                console.log("Error getting JCD station data " + response.statusCode);
             }
         });
     }
